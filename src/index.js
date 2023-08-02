59fea573f83888b407f2e1fb1c419ee22b4e0cd6
nikhil_777 <tandlenikhilraj@gmail.com>
2023-08-02 16:25:40 +0530

    Added analatics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './Pages/MainPage/Main';
-
+import { Analytics } from '@vercel/analytics/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Main />
+    <Analytics />
   </React.StrictMode>
 );
