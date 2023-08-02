1a635f7d6388d783697d57c16d7d713cdfec5829
nikhil_777 <tandlenikhilraj@gmail.com>
2023-08-02 12:06:03 +0530

    New Features thought of

---
+++ src/Components/ViewBased/DisplaySettings/EnableEdit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const EnableEdit = () => {
+  return (
+    <div>
+        <h1>Enable Edit</h1>
+        <h1>Also enable add automatically</h1>
+        <h1>Also enable delete automatically</h1>
+    </div>
+  )
+}
+
+export default EnableEdit
