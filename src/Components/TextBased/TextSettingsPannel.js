dd61e9f0d9d53de730c4bacf4dc1c2de8ead2856
nikhil_777 <tandlenikhilraj@gmail.com>
2023-08-04 12:27:03 +0530

    Added Load JSON option

--- src/Components/TextBased/TextSettingsPannel.js
+++ src/Components/TextBased/TextSettingsPannel.js
@@ -18,12 +18,28 @@ const TextSettingsPannel = ({ jsonData , setJsonData }) => {
         clearData(setJsonData)
     }
 
+    const handleLoadJSONFile = () => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.json';
+        input.onchange = e => {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.readAsText(file,'UTF-8');
+            reader.onload = readerEvent => {
+                setJsonData(readerEvent.target.result)
+            }
+        }
+        input.click();
+    }
+
 
     return (
         <Box sx={{ width: '100%', height: '100%', pb: 1 }}>
             <ButtonGroup variant="outlined" aria-label="outlined primary button group" size='secondary'>
                 <Button onClick={handleMinifyJSON}>Minify</Button>
                 <Button onClick={handleBeautifyJSON}>Beautify</Button>
+                <Button onClick={handleLoadJSONFile}>Load</Button>
                 <Button onClick={handleClearData}>Clear</Button>
             </ButtonGroup>
         </Box>
